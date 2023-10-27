42bf0d95e9a867c2f5545df2b9ccf5f607fc360a
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-10-27 23:32:41 +0200

    components dir created

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <div>
+            <p>This is where the Navbar goes</p>
+            <div>
+                <div>Hello</div>
+                <p>Hi</p>
+            </div>
+        </div>
+    )
+}
