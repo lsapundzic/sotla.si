36b0e18906ef28a3ecc8aa54f4dcfd1b06c23f5b
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-10-27 21:51:12 +0200

    testing if proj setup works

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,6 @@ export default function Navbar() {
     return (
         <div>
             <p>This is where the Navbar goes</p>
-            <div>
-                <div>Hello</div>
-                <p>Hi</p>
-            </div>
         </div>
     )
 }
