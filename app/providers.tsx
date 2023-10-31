9c8d4fb8bb7dea40333cd5448a32ed179f1ca132
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-10-31 15:15:55 +0100

    integrated chakra

---
+++ app/providers.tsx
@@ -0,0 +1,20 @@
+// For integrating Chakra UI into the project
+
+'use client'
+
+import {CacheProvider} from '@chakra-ui/next-js'
+import {ChakraProvider} from '@chakra-ui/react'
+
+export function Providers({
+                              children
+                          }: {
+    children: React.ReactNode
+}) {
+    return (
+        <CacheProvider>
+            <ChakraProvider>
+                {children}
+            </ChakraProvider>
+        </CacheProvider>
+    )
+}
