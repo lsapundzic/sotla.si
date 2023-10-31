3db198491709c200509d3007f734cdd605314378
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-10-31 17:11:08 +0100

    chakra is misperforming

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import {Button, ButtonGroup} from '@chakra-ui/react'
+import {Card, CardHeader, CardBody, CardFooter} from '@chakra-ui/react'
 
 export default function Home() {
     return (
         <div>
             <Button colorScheme="red">Hello</Button>
+            <Card>
+                <CardBody>
+                    <p>Hello</p>
+                </CardBody>
+            </Card>
         </div>
-
     )
 }
