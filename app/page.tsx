4b443fabadd0f73e1ad51119f697f0ae44ad357d
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-11-04 08:15:17 +0100

    installed prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
-import {Button, ButtonGroup} from '@chakra-ui/react'
-import {Card, CardHeader, CardBody, CardFooter} from '@chakra-ui/react'
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
 
 export default function Home() {
-    return (
-        <div>
-            <Button colorScheme="red">Hello</Button>
-            <Card>
-                <CardBody>
-                    <p>Hello</p>
-                </CardBody>
-            </Card>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <ol>
+        <li>HI</li>
+      </ol>
+    </div>
+  );
 }
