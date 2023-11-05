6a037974cee801008c03f73ce6a8ee8eb667480f
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-11-05 22:36:47 +0100

    Chakra works probably

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,26 @@
 import { Button, ButtonGroup } from "@chakra-ui/react";
-import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Text,
+  Spinner,
+} from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <div>
-      <h1>Welcome</h1>
-      <ol>
-        <li>HI</li>
-      </ol>
+      <h1>ddc</h1>
+
+      <Button>Hello</Button>
+
+      <Card>
+        <CardBody>
+          <Text>View a summary of all your customers over the last month.</Text>
+        </CardBody>
+      </Card>
+      <Spinner />
     </div>
   );
 }
